25f8ea71f1ea834e568512ebea58fe916db85e4b
Tsimafei <tima.kutsko@gmail.com>
2021-11-20 23:34:16 +0300

    HasSet

---
+++ Lesson4/BTree/Node.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTree
+{
+    class Node
+    {
+        public int Value { get; set; }
+        public Node RightChild { get; set; }
+        public Node LeftChild { get; set; }
+        public Node Root { get; set; }
+        
+        public Node (int v)
+        {
+            Value = v;
+        }
+
+        public Node Insert(Node nd, int v)
+        {
+            /*
+            Node insert(x : Node, z : T):
+                if x == null
+                    return Node(z)                        // подвесим Node с key = z
+                else if z < x.key
+                    x.left = insert(x.left, z)
+                else if z > x.key
+                    x.right = insert(x.right, z)
+                return x
+            */
+            if (nd == null)
+            {
+                return new Node(v);
+            }
+            else if (v < nd.Value)
+            {
+                nd.LeftChild = Insert(nd, v);
+            }
+            else if (v > nd.Value)
+            {
+                nd.RightChild = Insert(nd, v);
+            }
+            return nd;
+
+        }
+    }
+
+}
---
+++ Lesson4/Test/HashSetStr.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class HashSetStr : WordGen
+    {
+        public string RandString { get; set; }
+        
+        public override bool Equals(object obj)
+        {
+            var str = obj as HashSetStr;
+            if (str == null)
+                return false;
+            return RandString == str.RandString;
+        }
+
+        public override int GetHashCode()
+        {
+            int wordHashCode = RandString?.GetHashCode() ?? 0;
+            return wordHashCode;
+        }
+    }
+}
