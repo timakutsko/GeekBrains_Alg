8fedc2182ed517c2025bc244dfa43afba9a1d261
Tsimafei <tima.kutsko@gmail.com>
2021-11-21 20:38:41 +0300

    Графы

---
+++ Lesson6/Lesson6/Graph/Edge.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graph
+{
+    class Edge
+    {
+        public int Weight { get; set; } //вес связи
+        public Node Node { get; set; } // узел, на который связь ссылается
+        public Edge(Node node, int weight)
+        {
+            Node = node;
+            Weight = weight;
+        }
+    }
+
+}
